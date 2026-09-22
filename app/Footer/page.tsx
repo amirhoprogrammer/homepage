abe246fa3e437da536874380894c4f465e685da4
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2026-09-22 09:25:06 +0330

    add social icon and axios in project

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,6 +1,12 @@
 import FooterList from "@/components/FooterList";
 import FooterMap from "@/components/FooterMap";
 import { FooterLists } from "@/data/FooterLists";
+import Image from "next/image";
+const imageUrl = [
+  "/icons8-github-100.png",
+  "/icons8-instagram-100.png",
+  "/icons8-linkedin-100.png",
+];
 export default function Footer() {
   return (
     <div className=" pt-5">
@@ -12,7 +18,11 @@ export default function Footer() {
       <div className="border-t-2 border-black"></div>
       <div className="flex items-center justify-around">
         <p className="text-sm">©Copyright 2026. All Rights Reserved.</p>
-        <div className="flex gap-3"></div>
+        <div className="flex gap-3">
+          <Image src={imageUrl[0]} alt={"github"} width={50} height={50} />
+          <Image src={imageUrl[1]} alt={"instagram"} width={50} height={50} />
+          <Image src={imageUrl[2]} alt={"linkdin"} width={50} height={50} />
+        </div>
       </div>
     </div>
   );
